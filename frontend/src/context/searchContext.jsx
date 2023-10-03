3eb0398c7131a01e15a25a80e1448ec11c2e03fa
Vikas Pandey <vikas.pandey9323@gmail.com>
2023-10-03 14:37:30 +0530

    Update searchContext.jsx
    formatted

--- frontend/src/context/searchContext.jsx
+++ frontend/src/context/searchContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const SearchContext = createContext();
 
@@ -7,108 +7,86 @@ export const useSearch = () => {
 };
 
 export const SearchProvider = ({ children }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [filteredResults, setFilteredResults] = useState([]);
   const [ogMoviesList, setOGMoviesList] = useState([]);
 
   const [selectedOptions, setSelectedOptions] = useState([]);
   const [ogOptions, setogOptions] = useState([]);
 
-
   const handleSearch = (e) => {
-    console.log("object",ogOptions);
-    if (e.target.value === '' && selectedOptions.length === 0){
-        
-        setFilteredResults(ogMoviesList);
-    }else{
-
-      if (e.target.value === ''){
-        const valuesList = selectedOptions.map(item => item.value);
+    if (e.target.value === "" && selectedOptions.length === 0) {
+      setFilteredResults(ogMoviesList);
+    } else {
+      if (e.target.value === "") {
+        const valuesList = selectedOptions.map((item) => item.value);
         const newfilteredResults = ogMoviesList.map((entry) => ({
+          ...entry,
+          movies: entry.movies.filter((movie) =>
+            movie.genre.some((genre) => valuesList.includes(genre))
+          ),
+        }));
+        setFilteredResults(newfilteredResults);
+      } else {
+        let newfilteredResults = ogMoviesList;
+        if (selectedOptions.length !== 0) {
+          const valuesList = selectedOptions.map((item) => item.value);
+          newfilteredResults = ogMoviesList.map((entry) => ({
             ...entry,
             movies: entry.movies.filter((movie) =>
               movie.genre.some((genre) => valuesList.includes(genre))
-            )
-          }));
-          setFilteredResults(newfilteredResults);
-      }else{
-        let newfilteredResults = ogMoviesList;
-        if (selectedOptions.length !== 0){
-          const valuesList = selectedOptions.map(item => item.value);
-          newfilteredResults = ogMoviesList.map((entry) => ({
-              ...entry,
-              movies: entry.movies.filter((movie) =>
-                movie.genre.some((genre) => valuesList.includes(genre))
-              )
-            }));
-        }
-        
-
-         newfilteredResults = newfilteredResults.map((entry) => ({
-            date: entry.date,
-            movies: entry.movies.filter((movie) =>
-              movie.title.toLowerCase().includes(e.target.value.toLowerCase())
             ),
           }));
+        }
 
-        
-          setFilteredResults(newfilteredResults);
+        newfilteredResults = newfilteredResults.map((entry) => ({
+          date: entry.date,
+          movies: entry.movies.filter((movie) =>
+            movie.title.toLowerCase().includes(e.target.value.toLowerCase())
+          ),
+        }));
+
+        setFilteredResults(newfilteredResults);
       }
-
-     
-
     }
 
     setSearchQuery(e.target.value);
   };
 
-  const handleSelection = (val)=>{
-    console.log("searchQuery",searchQuery);
-    if (searchQuery === '' && val.length === 0){
-        
-        setFilteredResults(ogMoviesList);
-    }else{
-      console.log("object", searchQuery);
-      console.log("object", searchQuery==='');
-      if (searchQuery === ''){
-        const valuesList = val.map(item => item.value);
+  const handleSelection = (val) => {
+    if (searchQuery === "" && val.length === 0) {
+      setFilteredResults(ogMoviesList);
+    } else {
+      if (searchQuery === "") {
+        const valuesList = val.map((item) => item.value);
         const newfilteredResults = ogMoviesList.map((entry) => ({
-            ...entry,
-            movies: entry.movies.filter((movie) =>
-              movie.genre.some((genre) => valuesList.includes(genre))
-            )
-          }));
-          setFilteredResults(newfilteredResults);
-      }else{
-
-        const valuesList = val.map(item => item.value);
-      let newfilteredResults = ogMoviesList.map((entry) => ({
           ...entry,
           movies: entry.movies.filter((movie) =>
             movie.genre.some((genre) => valuesList.includes(genre))
-          )
+          ),
+        }));
+        setFilteredResults(newfilteredResults);
+      } else {
+        const valuesList = val.map((item) => item.value);
+        let newfilteredResults = ogMoviesList.map((entry) => ({
+          ...entry,
+          movies: entry.movies.filter((movie) =>
+            movie.genre.some((genre) => valuesList.includes(genre))
+          ),
         }));
 
-         newfilteredResults = newfilteredResults.map((entry) => ({
+        newfilteredResults = newfilteredResults.map((entry) => ({
           date: entry.date,
           movies: entry.movies.filter((movie) =>
             movie.title.toLowerCase().includes(searchQuery.toLowerCase())
           ),
         }));
 
-      
         setFilteredResults(newfilteredResults);
-
-       
       }
-        
-        
-       
-
     }
     setSelectedOptions(val);
-
-  }
+  };
 
   return (
     <SearchContext.Provider
@@ -123,7 +101,7 @@ export const SearchProvider = ({ children }) => {
         setFilteredResults,
         setOGMoviesList,
         setogOptions,
-        setSelectedOptions
+        setSelectedOptions,
       }}
     >
       {children}
